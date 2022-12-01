5111984a12e29865293916659676aa71d802326e
Perestra <danilo.ps.1996@gmai.com>
2022-12-01 16:27:18 -0300

    Added a darkmode function

---
+++ script/main.js
@@ -0,0 +1,5 @@
+const mode = document.querySelector('.imagens__content')
+const html = document.querySelector('html')
+mode.addEventListener('click', () => {
+    html.classList.toggle('dark-mode')
+} )
