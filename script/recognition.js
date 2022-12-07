d21c146a2c58e0020ad0c34076c05a3a6e44a5cf
Perestra <danilo.ps.1996@gmail.com>
2022-12-06 23:38:41 -0300

    Added some Functions to the recognition modal

--- script/recognition.js
+++ script/recognition.js
@@ -6,11 +6,47 @@ const recognitionButton = document.querySelector('.recognition_icon')
 const recognition = new SpeechRecognition()
 recognition.lang = 'pt-BR'
 
-recognitionButton.addEventListener('click', () => {
+
+function startRecognition() {
+    textRequest.innerHTML = 'Fale Agora'
+    recognitionContainer.classList.add('active')
     recognition.start()
+    timeOut2sec()
+    timeOut7sec()
+}
+function resultRecognition(e) {
+    const value = e.results[0][0].transcript
+    addHistoryList(value, generateId())
+    location.href = searchUrl.concat(value.split(' ').join('+'))
+}
+function stopRecognition() {
+    recognition.stop()
+    recognitionContainer.classList.remove('active')
+}
+
+const textRequest = document.getElementById('request')
+function timeOut2sec() {
+    setTimeout(() => {
+        textRequest.innerHTML = 'Ouvindo...'
+    }, 2000)
+}
+function timeOut7sec() {
+    setTimeout(() => {
+        textRequest.innerHTML = 'Não entendi o que você falou'
+    }, 7000)
+}
+
+
+const recognitionContainer = document.querySelector('.recognition_container')
+recognitionButton.addEventListener('click', () => {
+    startRecognition()
     recognition.onresult = e => {
-        const value = e.results[0][0].transcript
-        addHistoryList(value, generateId())
-        // location.href = searchUrl.concat(value.split(' ').join('+'))
+        resultRecognition(e)
     }
+    setTimeout(() => {
+        stopRecognition()
+    }, 10000)
 })
+
+const buttonRecognitionClose = document.querySelector('.recognition_close')
+buttonRecognitionClose.onclick = stopRecognition
